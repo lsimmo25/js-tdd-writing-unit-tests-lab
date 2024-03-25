519b749a8c29d7a89cb4c92be67f36525dfcb4f4
Lyle Simmons <lylesimmons125@gmail.com>
2024-03-24 21:00:59 -0400

    lab complete

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,19 @@
-// Your tests here
+const { isPalindrome } = require('../utils');
+
+describe('isPalindrome function', () => {
+  test('returns true for a palindrome word', () => {
+    expect(isPalindrome('racecar')).toBe(true);
+  });
+
+  test('returns false for a non-palindrome word', () => {
+    expect(isPalindrome('car')).toBe(false);
+  });
+
+  test('returns true for a palindrome with uppercase and lowercase letters', () => {
+    expect(isPalindrome('Racecar')).toBe(true);
+  });
+
+  test('returns false for an empty string', () => {
+    expect(isPalindrome('')).toBe(false);
+  });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,19 @@
-// Your code here
+function isPalindrome(word) {
+    if (typeof word !== 'string') {
+      throw new Error('Input must be a string');
+    }
+  
+    const cleanWord = word.toLowerCase().replace(/[^a-zA-Z]/g, '');
+  
+    if (!cleanWord) {
+      return false;
+    }
+  
+    if (!/^[a-zA-Z]+$/.test(cleanWord)) {
+      throw new Error('Input must contain only alphabetic characters');
+    }
+  
+    return cleanWord === cleanWord.split('').reverse().join('');
+  }
+
+  export { isPalindrome }
